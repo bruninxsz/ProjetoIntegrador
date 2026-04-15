4f34bab76d48dc0b2abf9e166e2ee36d84e894ea
bruninxsz <brunohsp0306@gmail.com>
2026-04-15 20:19:22 -0300

    Configuracao-inicial

---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+//importando o Vue Roter
+import {createRouter, createWebHistory} from 'vue-router';
+
+//Importando os componentes das páginas
+import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'; 
+import Dashboard from '../views/Dashboard.vue'; 
+
+
+
+//Definindo as rotas
+const routes = [
+    {
+        path:'/',                 // Rota para a página
+        component: Home           // Apelido para chamar depois
+    },
+
+    {
+        path:'/Login',
+        component: Login
+    },
+
+    {
+        path:'/Dashboard',
+        component: Dashboard
+    }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  scrollBehavior(to) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: 'smooth'
+      }
+    }
+    return { top: 0 }
+  }
+})
+
+export default router;
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col min-h-screen bg-gray-800 items-center font-bold text-3xl">
+    Landing Page
+  </div>
+</template>
+
+<script setup>
+</script>
+
