a937865fa86c7ab8bf19e566c16bc2a49cf15e13
-unset <-unset>
2026-04-23 22:55:36 -0300

    atualizacoes-home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,48 @@
 <template>
-  <div class="flex flex-col min-h-screen bg-gray-800 items-center font-bold text-3xl">
-    Landing Page
+  <div class="flex flex-col min-h-screen bg-gray-100 items-center font-bold text-3xl gap-32">
+    <header class="bg-white flex justify-between w-screen shadow-lg p-4 "">
+      <div>
+        <img src=" ../assets/logo.png" class="w-[140px] hover:scale-105">
+  </div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div class="text-gray-800 font-medium text-base my-auto hover:text-red-600">
+    Funcionalidades
+  </div>
+  <div class="text-gray-800 font-medium text-base my-auto hover:text-red-600">
+    Contato
+  </div>
+  <button class="bg-red-700 rounded-xl px-10 text-white h-[50px] my-auto font-medium text-base hover:scale-[1.02]">
+    Entrar
+  </button>
+  </header>
+
+  <div class="flex justify-between text-left gap-8">
+    <div class="flex flex-col gap-8">
+      <div class=" text-red-600 text-8xl">
+        Gestão <br> Inteligente <br>de EPIs
+      </div>
+      <div class="text-xl font-medium text-left">
+        SGEE é a solução completa para controle, <br>
+        distribuição e conformidade de <br>
+        Equipamentos de Proteção Individual.<br>
+        Proteja seus colaboradores com <br>
+        tecnologia moderna e conformidade <br>regulatória garantida.
+      </div>
+    </div>
+    <div>
+      <img src="../assets/home/landingimg.png" class="mt-24">
+    </div>
+  </div>
+
+
+
+
   </div>
 </template>
 
 <script setup>
 </script>
-
