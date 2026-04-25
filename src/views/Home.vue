88dbc15713640f510165502ca8556bf6bf486abf
bruninxsz <brunohsp0306@gmail.com>
2026-04-24 22:58:27 -0300

    continuacao-landingpage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,45 +1,103 @@
 <template>
-  <div class="flex flex-col min-h-screen bg-gray-100 items-center font-bold text-3xl gap-32">
-    <header class="bg-white flex justify-between w-screen shadow-lg p-4 "">
+  <div class="flex flex-col min-h-screen bg-gray-100 items-center font-bold text-3xl gap-48">
+
+    <header class="sticky top-0 bg-white flex justify-between w-full shadow-lg p-4 z-50">
       <div>
         <img src=" ../assets/logo.png" class="w-[140px] hover:scale-105">
-  </div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div class="text-gray-800 font-medium text-base my-auto hover:text-red-600">
-    Funcionalidades
-  </div>
-  <div class="text-gray-800 font-medium text-base my-auto hover:text-red-600">
-    Contato
-  </div>
-  <button class="bg-red-700 rounded-xl px-10 text-white h-[50px] my-auto font-medium text-base hover:scale-[1.02]">
-    Entrar
-  </button>
-  </header>
-
-  <div class="flex justify-between text-left gap-8">
-    <div class="flex flex-col gap-8">
-      <div class=" text-red-600 text-8xl">
-        Gestão <br> Inteligente <br>de EPIs
       </div>
-      <div class="text-xl font-medium text-left">
-        SGEE é a solução completa para controle, <br>
-        distribuição e conformidade de <br>
-        Equipamentos de Proteção Individual.<br>
-        Proteja seus colaboradores com <br>
-        tecnologia moderna e conformidade <br>regulatória garantida.
+
+      <div class="flex justify-between gap-12">
+        <div class="text-gray-800 font-medium text-base my-auto hover:text-red-600">
+          Funcionalidades
+        </div>
+        <div class="text-gray-800 font-medium text-base my-auto hover:text-red-600">
+          Contato
+        </div>
+        <button
+          class="bg-red-700 rounded-xl px-10 text-white h-[50px] my-auto font-medium text-base hover:scale-[1.02]">
+          Entrar
+        </button>
       </div>
-    </div>
-    <div>
-      <img src="../assets/home/landingimg.png" class="mt-24">
-    </div>
-  </div>
+
+    </header>
 
 
 
+
+    <div class="flex flex-col">
+      <div class="flex justify-between text-left gap-8">
+        <div class="flex flex-col gap-12">
+          <div class=" text-red-600 text-8xl max-w-2xl">
+            Gestão Inteligente de EPIs
+          </div>
+          <div class="text-xl font-medium text-left max-w-2xl">
+            SGEE é a solução completa para controle, distribuição e conformidade de Equipamentos de Proteção Individual.
+            Proteja seus colaboradores com tecnologia moderna e conformidade regulatória garantida.
+          </div>
+        </div>
+        <div>
+
+          <img src="../assets/home/landingimg.png" class="mt-16 w-[650px]">
+        </div>
+      </div>
+      <div class="flex justify between mt-4 gap-12">
+        <div>
+          <button
+            class="bg-red-700 rounded-xl px-10 text-white h-[50px] my-auto font-medium text-base hover:scale-[1.02]">
+            Entrar
+          </button>
+        </div>
+        <div>
+          <button
+            class="bg-red-700 rounded-xl px-10 text-white h-[50px] my-auto font-medium text-base hover:scale-[1.02]">
+            Saiba mais
+          </button>
+        </div>
+      </div>
+      </div>
+
+
+
+      <div class="flex flex-col mx-auto gap-12 text-center">
+        <div class="text-6xl text-red-600 font-bold">
+          Funcionalidades Principais
+        </div>
+        <div class="text-2xl text-gray-800 font-medium max-w-3xl">
+          Tudo que você precisa para gerenciar EPIs de forma eficiente e em conformidade com a legislação
+        </div>
+      </div>
+
+      <div class="flex flex-col mx-auto text-center gap-8 text-">
+        <div class="flex justify-center gap-12">
+          <div class="flex flex-col p-6 bg-white rounded-xl shadow-md">
+              <div class="bg-red-200 rounded-md p-2 w-[50px]">
+                <img src="../assets/home/icon1.svg" class="w-10">
+            </div>
+            <div class="">Controle de Estoque</div>
+          </div>
+          <div class="p-6 bg-white rounded-xl shadow-md">
+            asda
+          </div>
+          <div class="p-6 bg-white rounded-xl shadow-md">
+            asda
+          </div>
+        </div>
+
+        <div class="flex justify-center gap-12">
+          <div class="p-6 bg-white rounded-xl shadow-md">
+            asda
+          </div>
+          <div class="p-6 bg-white rounded-xl shadow-md">
+            asda
+          </div>
+          <div class="p-6 bg-white rounded-xl shadow-md">
+            asda
+          </div>
+        </div>
+      </div>
+
+
+   
 
   </div>
 </template>
